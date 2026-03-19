fe10a98e3bc2b125ec7f0ce334ceb630ce9e0bba
acelion408 <fightlion422@gmail.com>
2026-03-19 17:52:31 +0530

    Update router

--- router.php
+++ router.php
@@ -2,7 +2,7 @@
 // CORS headers
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-HTTP-Method-Override');
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
